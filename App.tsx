7614c5efb63ce7077269fb9ca461372fd23fcc98
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2021-02-25 18:11:14 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{instruction}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const instruction = Platform.select({
+  ios: "This is ios",
+  android: "this is andriod"
+})
 
 const styles = StyleSheet.create({
   container: {
